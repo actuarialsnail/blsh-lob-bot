21818fab0e043b96692653c055c425ef9ca70fa6
actuarialsnail <flynn.yiyi@gmail.com>
2021-03-20 11:45:50 +0000

    automation consol data script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 rsync -aP --compare-dest=/home/yi/Documents/arbitor/blsh-lob-bot/lob-databot/binance-eth_btc-archived/  yi@192.168.0.42:/home/yi/lob-databot/logs/ ./lob-databot/binance-eth_btc/
 python dataset_compile.py
+python dataset_consol.py
 mv -v ./lob-databot/binance-eth_btc/* ./lob-databot/binance-eth_btc-archived
 read
